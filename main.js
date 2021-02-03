5805b8457cda8c33b88fddbbba3756c297b2cdd4
amazuhina <a.mazuhina@mail.ru>
2021-02-03 21:04:52 +0400

    init

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
 
-/*
+//определяем кнопку
 let button = document.querySelector('.button')
 
+//определяем данные инпута
+let input = document.querySelector('.input-message')
 
-function getMessage () {
-    let message = document.querySelector('.input').value
+//определяем блок сообщения в окне диалога
+let textMessage = document.querySelector('.text')
+
+//определяем блок для времени
+let $time = document.querySelector('.time')
+
+//функция добавлять еще один блок сообщения к старым
+function anotherMessage (){
+    let newDiv ="<div class='message'><div class='text'></div><div class='time'></div></div>"
+    document.querySelector('.message').innerHTML += newDiv
+}
+//Создаем нужный формат времени
+    let date = new Date()
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    }
+    let optionsDate = date.toLocaleString("ru", options)
+
+// Показать сообщение
+function showMessage() {
+    let noShow = document.querySelector('.message')
+    noShow.style.display = 'block'
+
 }
 
+//функция после клика вставлять в блок сообщения инпут
 
-function showDialogue() {
-    let one = document.querySelector('.one')
-    one.textContent = message
-}
-*/
-
-let button = document.querySelector('.button')
 
 button.addEventListener('click', function (event) {
-    let message = document.querySelector('.text-message').value
-    let one = document.querySelector('.one')
-    one.textContent = message
+    showMessage()
+    textMessage.textContent = input.value
+    $time.textContent = optionsDate
+    input.value = ""
+    anotherMessage()
 
-
-    
 })
 
+
+
+/* вопросики:
+   1) ну в целом чат не работает как должен
+   2) как переносить строчку в блоке месседж и инпуте, если сообщение длинное
+   3)нет полосы прокрутки
+   4) див с месседж ушел влево зачем то
+ */
